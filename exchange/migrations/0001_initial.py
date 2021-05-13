c714ce0f3da0b72faea699d18f2c3d18eb26d9ee
Faneva Andriamiadantsoa <fanevanjanahary@gmail.com>
2021-05-13 22:15:09 +0300

    migrate to django 2

---
+++ exchange/migrations/0001_initial.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Currency',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('code', models.CharField(max_length=3, unique=True)),
+                ('name', models.CharField(max_length=64)),
+            ],
+            options={
+                'verbose_name_plural': 'currencies',
+            },
+        ),
+        migrations.CreateModel(
+            name='ExchangeRate',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('rate', models.DecimalField(decimal_places=8, max_digits=17)),
+                ('source', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='rates', to='exchange.Currency')),
+                ('target', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='exchange.Currency')),
+            ],
+        ),
+    ]
---
+++ exchange/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from exchange.managers import ExchangeRateManager
+from exchange.iso_4217 import code_list
+
+
+class Currency(models.Model):
+    """Model holds a currency information for a nationality"""
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name_plural = 'currencies'
+
+    def __unicode__(self):
+        return self.code
+
+    def get_numeric_code(self):
+        return code_list[self.code]  # Let it raise an exception
+
+
+class ExchangeRate(models.Model):
+    """Model to persist exchange rates between currencies"""
+    source = models.ForeignKey(Currency, related_name='rates', on_delete=models.CASCADE)
+    target = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    rate = models.DecimalField(max_digits=17, decimal_places=8)
+
+    objects = ExchangeRateManager()
+
+    def __unicode__(self):
+        return '%s / %s = %s' % (self.source, self.target, self.rate)
